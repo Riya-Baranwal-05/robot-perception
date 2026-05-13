d034826e2e588114d5c8d643e9c3b1fc160eb5c3
Riya Baranwal <ria.baranwal@gmail.com>
2026-05-13 15:38:53 -0400

    Implemented train/eval.py with mAP evaluation

--- train/eval.py
+++ train/eval.py
@@ -1,9 +1,14 @@
 """
 This module is going to test the trained model using test dataset.
 """
-def eval_model():
+from ultralytics import YOLO 
+
+def eval_model(model,data_yaml_path):
     """
     This is going to take test dataset and the fine tuned model 
     and validate it using mAP metrics.
     """
-    pass
+    results = model.val(data=data_yaml_path)
+    print(results.box.map)
+    print(results.box.map50)
+    return results
