9ffc3446824b27cb337f0d76d6d80a108d7a4328
Riya Baranwal <ria.baranwal@gmail.com>
2026-05-12 23:09:11 -0400

    Implemented data/download.py with environment variable for API key

--- data/download.py
+++ data/download.py
@@ -8,4 +8,12 @@ def download_dataset():
     This downloads the dataset from Roboflow using the API.
     It is going to save it in local data directory in YOLOv8 format.
     """
-    pass
+    
+
+    from roboflow import Roboflow
+    api_key = os.environ.get("ROBOFLOW_API_KEY")
+    rf = Roboflow(api_key=api_key)
+    project = rf.workspace("household-j10i9").project("household-khbd4")
+    version = project.version(1)
+    dataset = version.download("yolov8")
+                
