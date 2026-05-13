3c2493d56e20bdf109d44bfcfba2095979324fe1
Riya Baranwal <ria.baranwal@gmail.com>
2026-05-13 15:53:49 -0400

    Implemented ros2_node/detector_node.py with YOLOv8 inference and ROS2 publisher

--- ros2_node/detector_node.py
+++ ros2_node/detector_node.py
@@ -2,9 +2,27 @@
 This module is a ROS node that subscribes to camera images and 
 runs YOLOv8 model on each image and publishes the detection results.
 """
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from ultralytics import YOLO
 
-def detect():
+class DetectorNode(Node):
     """
     This is going to read subscribed images , feed them to fine tuned model
     Publishes the class of the object , bounding box ,confidence score
     """
+    def __init__(self):
+        super().__init__('detector_node')
+        self.subscription=self.create_subscription(Image,'/camera/image_raw',self.detect,10)
+        self.publisher_ = self.create_publisher(String,'/detections',10)
+        self.model = YOLO('best.pt')
+
+    def detect(self,msg):
+        msg = self.model(msg)
+        self.publisher_.publish(msg)
+
+
+    
+    
